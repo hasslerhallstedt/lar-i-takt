ba4864e62c4785c7a4ef41144428b38a160265f3
Martin <hasslerhallstedt@gmail.com>
2026-02-13 21:08:17 +0100

    wip

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 # Assets and data
 #CSV_FILENAME = "resources/nyfil_gang-1.csv"
-CSV_FILENAME = "resources/Content_Avicii.json"
+CSV_FILENAME = "resources/Content_Tyska_glosor_Master.json"
 POP_SOUND = "resources/pop.wav"
 MUSIC_SOUND = "resources/Avicii.mp3"
 POSE_MODEL_PATH = os.getenv("POSE_LANDMARKER_MODEL_PATH", "pose_landmarker_full.task")
 FACE_MODEL_PATH = os.getenv("FACE_LANDMARKER_MODEL_PATH", "face_landmarker.task")
-FONT_SIZE = 96
+FONT_SIZE = 48
 FONT_PATH = "resources/Roboto-Black.ttf"
 BACKGROUND_IMAGE = "resources/background.jpg"
 SHOW_FACE_MESH = True
